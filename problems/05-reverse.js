2522c76bdae142743a9893272974a8aeb288a56c
Alex Carl <alexjcarl97@gmail.com>
2023-09-20 18:26:52 -0400

    solved 1

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -14,9 +14,19 @@ reverse(""); // ""
 
 
 function reverse(str) {
-  // Your code here 
+
+if(str.length === 0) return str;
+
+return str.slice(str.length - 1)+ reverse(str.slice(0, str.length - 1))
+
 }
 
+console.log(reverse("house")); // "esuoh"
+console.log(reverse("dog")); // "god"
+console.log(reverse("atom")); // "mota"
+console.log(reverse("q")); // "q"
+console.log(reverse("id")); // "di"
+console.log(reverse("")); // ""
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
